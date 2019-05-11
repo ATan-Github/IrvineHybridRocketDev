a3afd0de0d06a77e37d2aa6a2f444f62ea5a23c6
Darren Tan <darren.tan@spacex.com>
2019-05-10 20:57:24 -0700

    Created thermistor code for resistance reading

---
+++ data_ctrl/thermistor/thermistor.ino
@@ -0,0 +1,50 @@
+// which analog pin to connect
+#define THERMISTORPIN A0         
+// resistance at 25 degrees C
+#define THERMISTORNOMINAL 100000      
+// temp. for nominal resistance (almost always 25 C)
+#define TEMPERATURENOMINAL 25   
+// how many samples to take and average, more takes longer
+// but is more 'smooth'
+#define NUMSAMPLES 5
+// The beta coefficient of the thermistor (usually 3000-4000)
+#define BCOEFFICIENT 3950
+// the value of the 'other' resistor
+#define SERIESRESISTOR 9960  
+void setup(void) {
+  Serial.begin(9600);
+}
+ 
+void loop(void) {
+  float reading;
+ 
+  reading = analogRead(THERMISTORPIN);
+ 
+  Serial.print("Analog reading "); 
+  Serial.println(reading);
+ 
+  // convert the value to resistance
+  reading = (1023 / reading)  - 1;     // (1023/ADC - 1) 
+  reading = SERIESRESISTOR / reading;  // 10K / (1023/ADC - 1)
+  Serial.print("Thermistor resistance "); 
+  Serial.println(reading);
+
+
+ /* FIND A BETTER RESISTANCE TO TEMPERATURE CONVERSION FOR 100K THERMISTOR (data sheet below)
+  *  
+  *  https://www.makeralot.com/download/Reprap-Hotend-Thermistor-NTC-3950-100K.pdf
+  *  
+  float steinhart;
+  steinhart = reading / THERMISTORNOMINAL;     // (R/Ro)
+  steinhart = log(steinhart);                  // ln(R/Ro)
+  steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
+  steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
+  steinhart = 1.0 / steinhart;                 // Invert
+  steinhart -= 273.15;                         // convert to C
+  steinhart = ((9/5)*steinhart) + 32;
+  Serial.print("Temperature "); 
+  Serial.print(steinhart);
+  Serial.println(" *F");
+  */
+ 
+}
